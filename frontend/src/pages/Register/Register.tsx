e71d69b746859921f4e13c4229ebdaa7e8524daf
Atishaya Maharjan <71453175+Atishaya7777@users.noreply.github.com>
2025-02-21 23:15:20 -0600

    feat: Added styling for the registration page

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,39 +1,60 @@
-import { useState } from 'react'
 import { useRegister } from 'api/auth';
 import { useUserStore } from 'store/useUserStore';
+import { useEffect } from "react";
+import { Button, Input } from "components";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+import tokenService from 'utils/token';
+import { routePaths } from "global/routePaths";
+
+interface RegisterFormData {
+	username: string;
+	password: string;
+}
 
 const Register = () => {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
+	const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
 
 	const addUserDetails = useUserStore(state => state.addUserDetails);
 
-	const { mutate: register } = useRegister();
+	const { mutate: registerMutate } = useRegister();
 
-	const updateUsername = (e: any) => {
-		setUsername(e.target.value);
+	const onSubmit = (data: RegisterFormData) => {
+		addUserDetails(data);
+		registerMutate(data);
 	}
 
-	const updatePassword = (e: any) => {
-		setPassword(e.target.value);
-	}
-
-	const submitBtnClicked = (e: any) => {
-		e.preventDefault();
-		addUserDetails({ username, password });
-		register({ username, password });
-	}
+	useEffect(() => {
+		if (tokenService.getAccessToken()) {
+			navigate(routePaths.globalChat);
+		}
+	}, [])
 
 	return (
-		<div>
-			<h1>Register</h1>
-			<form>
-				<label>User:</label>
-				<input type="text" id="username" name="Username" onChange={updateUsername}></input>
-				<label>Password:</label>
-				<input type="text" id="password" name="Password" onChange={updatePassword}></input>
-				<button onClick={submitBtnClicked}>Register</button>
-			</form>
+		<div className="register-container">
+			<div className="bg-gray-800/80 text-xl p-6 rounded-lg w-[75%] text-gray-100">
+				<h1 className="text-3xl mb-5">Register to avoid touching grass!</h1>
+				<form className="flex flex-col justify-center text-2xl" onSubmit={handleSubmit(onSubmit)}>
+					<div className="flex gap-15 mb-2">
+						<label className="mr-3">User:</label>
+						<div className="w-full">
+							<Input className="font-bold" type="text" id="username" {...register("username", { required: true })} />
+							{errors.username && <span className="ml-4 text-lg text-red-700">This field is required</span>}
+						</div>
+					</div>
+					<div className="flex gap-3 mb-5">
+						<label>Password:</label>
+						<div className="w-full">
+							<Input type="text" id="password" {...register("password", { required: true })} />
+							{errors.password && <span className="ml-4 text-lg text-red-700">This field is required</span>}
+						</div>
+					</div>
+					<div className="flex justify-center">
+						<Button type="submit">Register</Button>
+					</div>
+				</form>
+			</div>
 		</div>
 	)
 }
